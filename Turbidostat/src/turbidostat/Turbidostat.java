a880c1aa22d3d9c86a2f06cd299e4c5cdf5792d3
Jase <jlgrills@uw.edu>
2017-07-23 18:02:42 -0700

    Fresh NB Project
    Fresh NB Project, No code.

---
+++ Turbidostat/src/turbidostat/Turbidostat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turbidostat;
+
+/**
+ *
+ * @author jason
+ */
+public class Turbidostat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
