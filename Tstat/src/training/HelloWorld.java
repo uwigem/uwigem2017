0932e48b568d42e9e2807b0333b787fc38197292
Fedor <fpogulsky@live.com>
2017-07-26 12:48:37 -0700

    h
    hi

--- Tstat/src/training/HelloWorld.java
+++ Tstat/src/training/HelloWorld.java
@@ -23,10 +23,6 @@ package training;
 public class HelloWorld {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World");
-    public static void main(String[] args) {
-        System.out.println("Hello World Take 106 - Stephen");
-        System.out.println("text");
         
     }
 }
