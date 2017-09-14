f4dfb88c9f0804841d8d3afcb5477efb1e56c2c9
mockingod <mockingod@gmail.com>
2017-09-14 12:38:31 -0700

    Updated main Tstat file
    - Added area to write pins of for each pump
    - Added intialization procedures

--- Tstat/src/tstat/Tstat.java
+++ Tstat/src/tstat/Tstat.java
@@ -17,11 +17,12 @@
 
 package tstat;
 
-import java.util.Scanner;
+import java.util.*;
 import java.lang.*;
 import com.pi4j.io.gpio.*;
 import com.pi4j.util.CommandArgumentParser;
 import com.pi4j.util.Console;
+
 
 
 /**
@@ -31,12 +32,42 @@ import com.pi4j.util.Console;
  * @author Washington iGEM Team 2017
  */
 public class Tstat {
-
+    
+    // Pins for each specific part of the pump listed in order: pump 0, 1, 2
+    // TODO: All these pins are placeholder pins
+    public static final Pin[] DIR_PINS = {RaspiPin.GPIO_29, RaspiPin.GPIO_30, RaspiPin.GPIO_31};
+    public static final Pin[] STEP_PINS  = {RaspiPin.GPIO_26, RaspiPin.GPIO_27, RaspiPin.GPIO_28};
+    public static final Pin[] ENABLE_PINS = {RaspiPin.GPIO_29, RaspiPin.GPIO_30, RaspiPin.GPIO_31};
+    public static final Pin[] MAX_STOP_PINS = {RaspiPin.GPIO_29, RaspiPin.GPIO_30, RaspiPin.GPIO_31};
+    public static final Pin[] MIN_STOP_PINS = {RaspiPin.GPIO_29, RaspiPin.GPIO_30, RaspiPin.GPIO_31};
+    public static final double DEFAULT_RATE = 1.0;
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        
         System.out.println("Turbidostat Application Run");
+        System.out.println("Setting up Raspberry Pi and Syringe Pumps");
+        
+        final GpioController gpio = GpioFactory.getInstance();
+        Fluids fluids = new Fluids();
+        for(int i = 0; i < 3; i++) {
+            SyringePump addPump = new SyringePump(gpio.provisionDigitalOutputPin(DIR_PINS[i]), 
+                                                    gpio.provisionDigitalOutputPin(STEP_PINS[i]), 
+                                                    gpio.provisionDigitalOutputPin(ENABLE_PINS[i]), 
+                                                    gpio.provisionDigitalInputPin(MAX_STOP_PINS[i]), 
+                                                    gpio.provisionDigitalInputPin(MIN_STOP_PINS[i]), 
+                                                    DEFAULT_RATE);
+        }
+        
+        System.out.println("Calibrating pumps");
+        
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Calibrating pump 1");
+            fluids.calibrateFrom(i);
+        }
+        
+        
     }
-
 }
